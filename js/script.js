be5a1f6533bafaff4a2e5abffd6fbe5a54baf5a9
Bruno Anderson S.F <bruno.asf2021@gmail.com>
2022-10-21 21:08:53 -0300

    alteração no slider

--- js/script.js
+++ js/script.js
@@ -7,3 +7,29 @@ function mostrarMenu() {
         menu.style.display = "none";
     }
 }
+
+
+let slideAtual = 1;
+
+let listaSlide = ["banner-gamer", "banner-playstation", "banner-xbox", "banner-nintendo"]
+
+setInterval(slide, 2500)
+
+function slide(){
+
+    console.log("slide Atual", slideAtual)
+
+    if(slideAtual > 0){
+        document.querySelector("#carrossel").classList.remove(listaSlide[slideAtual - 1])
+    } else if (slideAtual == 0) {
+        document.querySelector("#carrossel").classList.remove(listaSlide[(listaSlide.length) - 1])
+    }
+
+    document.querySelector('#carrossel').classList.add(listaSlide[slideAtual])
+
+    slideAtual++;
+
+    if(slideAtual > (listaSlide.length) - 1){
+        slideAtual = 0;
+    }
+}
